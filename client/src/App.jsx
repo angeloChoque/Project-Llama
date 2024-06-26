146a37f25ccdcd5c38924b47731cc65cfc312b12
angelo <ancricoma_angelo@hotmail.com>
2024-06-25 23:44:31 -0500

    added color and header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,26 @@
-import DrawerHome from '../src/pages/home/DrawerHome';
-import Home from '../src/pages/home/Home';
-import { Box, Dialog, Fab, ListItemButton, ListItemIcon, Popover } from '@mui/material';
-import { Route, Routes } from 'react-router-dom';
-import Header from '../src/components/header/Header';
-import EmployeeData from '../src/pages/employeeData/EmployeeData';
-import Calendar from '../src/pages/calendar/Calendar';
-import { EmployeeContext } from '../src/context/EmployeeContext';
-import InformationPage from '../src/pages/infoEmployee/InformationPage';
-import TaskDragAndDrop from '../src/pages/task/TaskDragAndDrop';
-import HelpIcon from '@mui/icons-material/Help';
-import { useState } from 'react';
-import EmailDialog from '../src/components/alerts/EmailDialog';
-import AboutPage from './pages/aboutPage/AboutPage';
+import DrawerHome from "../src/pages/home/DrawerHome";
+import Home from "../src/pages/home/Home";
+import {
+  Box,
+  Dialog,
+  Fab,
+  ListItemButton,
+  ListItemIcon,
+  Popover,
+  Stack,
+  Typography,
+} from "@mui/material";
+import { Route, Routes } from "react-router-dom";
+import Header from "../src/components/header/Header";
+import EmployeeData from "../src/pages/employeeData/EmployeeData";
+import Calendar from "../src/pages/calendar/Calendar";
+import { EmployeeContext } from "../src/context/EmployeeContext";
+import InformationPage from "../src/pages/infoEmployee/InformationPage";
+import TaskDragAndDrop from "../src/pages/task/TaskDragAndDrop";
+import HelpIcon from "@mui/icons-material/Help";
+import { useState } from "react";
+import EmailDialog from "../src/components/alerts/EmailDialog";
+import AboutPage from "./pages/aboutPage/AboutPage";
 
 function App() {
   const [showPopover, setShowPopover] = useState(false);
@@ -26,6 +35,16 @@ function App() {
   return (
     <>
       <DrawerHome>
+        <Stack sx={{ bgcolor: "rgb(255, 165, 0)" }}>
+          <Typography
+            textAlign={"center"}
+            pb={0.5}
+            fontSize={18}
+            color={"white"}
+          >
+            Wait for the API response. This may take 2 to 3 minutes.{" "}
+          </Typography>
+        </Stack>
         <Header />
         <EmployeeContext>
           <Routes>
@@ -37,48 +56,44 @@ function App() {
             <Route path="/about" element={<AboutPage />} />
           </Routes>
         </EmployeeContext>
-        <Fab 
-          color="primary" 
-          style={{ position: 'absolute', bottom: 16, right: 35 }}
+        <Fab
+          color="primary"
+          style={{ position: "absolute", bottom: 16, right: 35 }}
           onClick={handlePopper}
         >
           <HelpIcon />
         </Fab>
         <Popover
-          sx={{ transform: 'translate(-30px, -5px)' }}
+          sx={{ transform: "translate(-30px, -5px)" }}
           open={showPopover}
           anchorEl={anchorEl}
           onClose={() => setShowPopover(false)}
           anchorOrigin={{
-            vertical: 'top',
-            horizontal: 'center',
+            vertical: "top",
+            horizontal: "center",
           }}
           transformOrigin={{
-            vertical: 'bottom',
-            horizontal: 'center',
+            vertical: "bottom",
+            horizontal: "center",
           }}
         >
-          <Box
-          width={"230px"}
-          >
-            <ListItemButton 
-            sx={{py:1.5}}
-            onClick={() => setShowDialog(true)}
+          <Box width={"230px"}>
+            <ListItemButton
+              sx={{ py: 1.5 }}
+              onClick={() => setShowDialog(true)}
             >
               <ListItemIcon>
-                <HelpIcon  />
+                <HelpIcon />
               </ListItemIcon>
               Contact
             </ListItemButton>
           </Box>
           <Dialog
-          open={showDialog} 
-          onClose={() => setShowDialog(false)} 
-          maxWidth="xs"
+            open={showDialog}
+            onClose={() => setShowDialog(false)}
+            maxWidth="xs"
           >
-            <EmailDialog
-              onclose={()=>setShowDialog(false)}
-            />
+            <EmailDialog onclose={() => setShowDialog(false)} />
           </Dialog>
         </Popover>
       </DrawerHome>
